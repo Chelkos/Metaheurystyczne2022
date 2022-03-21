15fe9b3fcdfa5050996d6c697897e581c1ef5c3c
Chelkos <piotr.zuk.pl@gmail.com>
2022-03-21 21:04:21 +0100

    Added k_random

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,8 @@
+from logging import exception
 import networkx as nx
 import math
-import random
+import random 
+
 
 def objective(G):
     return G.size(weight='weight')
@@ -34,4 +36,31 @@ def repetitive_nearest_neighbour(G):
             solution = route
             min = weight
 
+    return solution
+
+
+def k_random(k,G):
+    solution = nx.empty_graph()
+    solution_weight = None
+    for i in range(k):
+        possible_solution = nx.empty_graph()
+        graph = G.copy()
+        random_node = random.choice(list(graph.nodes()))
+        
+        for n in range(len(graph.nodes())-1):
+            possible_solution.add_node(random_node)
+            random_edge = random.choice(list(graph.edges(random_node)))
+            (u, v) = random_edge
+            edge_weight =  graph[u][v]['weight']
+            possible_solution.add_node(v)
+            possible_solution.add_edge(u,v,weight = edge_weight)
+            graph.remove_node(u)
+            random_node = v
+            
+        if solution_weight is None or objective(possible_solution)<solution_weight:
+            
+            solution =possible_solution
+            solution_weight = objective(possible_solution)
+    
+    
     return solution
