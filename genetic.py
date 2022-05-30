79ff16db8e66ba24dbf72ae2ca15986d6bcb518b
Phaser <piotr.piszczek666@gmail.com>
2022-05-30 15:08:04 +0200

    genetic algorithm first version

---
+++ genetic.py
@@ -0,0 +1,87 @@
+import algorithms as algs
+import networkx as nx
+import random
+import utils as utils
+
+def generate_population(G, alg=algs.k_random, pop_size =10):
+    population = []
+
+    for n in range(pop_size):
+        sol = alg(G)
+        weight = utils.objective(sol)
+        population.append((sol,weight))
+        print(sol.nodes()," ",weight)
+    selection(G, population,pop_size)
+    
+
+#def generate_tuples(population):
+
+
+
+def selection(G, population, pop_size, elite_size=2):
+    parents = []
+    prob = []
+    maxi = population[0][1]
+    sum = 0
+
+    for i in range(len(population)):
+        if maxi < population[i][1]:
+            maxi = population[i][1]
+        sum+=1/population[i][1]
+
+    for i in range(len(population)):
+        prob.append((1/population[i][1])/sum)
+
+    while len(parents)<pop_size/2:
+        for i in range(len(population)):
+            if random.randrange(0,1)<prob[i] and population[i] not in parents:
+                parents.append(population[i])
+                print(population[i][0].nodes())
+                if len(parents)>=pop_size/2:
+                    break
+    breed(G, parents)
+    return parents
+
+
+def single_breed(G,parent1,parent2,start,end):
+
+
+    print(start, end)
+    child = nx.DiGraph()
+    
+    for j in range(start, end):
+        child.add_node(list(parent1)[j])
+    
+    for j in range(end, len(parent2)):
+        if list(parent2)[j] not in list(child.nodes()):
+            child.add_node(list(parent2)[j])
+
+    for j in range(0, end):
+        if list(parent2)[j] not in list(child.nodes()):
+            child.add_node(list(parent2)[j])
+
+    nodes = list(child.nodes())
+    for n in range(len(nodes) - 1):
+        child.add_edge(nodes[n], nodes[n+1], weight=G[nodes[n]][nodes[n+1]]['weight'])
+    
+    child.add_edge(nodes[0], nodes[len(G.nodes())-1], weight=G[nodes[0]][nodes[len(G.nodes())-1]]['weight'])
+    return (child,utils.objective(child))
+
+
+def breed(G, parents):
+    new_pop = []
+    for i in range(0, len(parents)-1, 2):
+        parent1 = parents[i][0].nodes()
+        parent2 = parents[i+1][0].nodes()
+        
+        geneA = random.randint(0, len(parent1)-1)
+        geneB = random.randint(0, len(parent2)-1)
+
+        start = min(geneA, geneB)
+        end = max(geneA, geneB)
+        new_pop.append(single_breed(G,parent1,parent2,start,end))
+        new_pop.append(single_breed(G,parent2,parent1,start,end))
+        new_pop.append(parents[i])
+        new_pop.append(parents[i+1])
+        print(i)
+    #print(new_pop)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tracemalloc import start
 import tsplib95 as tsp
 import algorithms as alg
 import generators as gen
@@ -6,6 +5,8 @@ import random
 import utils
 import networkx as nx
 import time
+import genetic as gene
+
 def load(file):
     problem = tsp.load(file)
     G = problem.get_graph()
@@ -32,16 +33,17 @@ def test_alg_type():
         alg.swap,
         alg.insert,
     ]
-    for a in algs:
-        if a == alg.invert:
-            n = "invert"
-        elif a==alg.insert:
-            n = "insert"
-        elif a==alg.swap:
-            n = "swap"
-        f_w = open(str('tabu_'+n+'.txt'),"w")
-        for size in [10,20,30,40]:
-            curr_graph = gen.generate_symmetric(size,1000)
+    for size in [10,20,30,40,50,60,70,80,90,100]:
+        curr_graph = gen.generate_asymmetric(size,1000)
+        for a in algs:
+            if a == alg.invert:
+                n = "invert"
+            elif a==alg.insert:
+                n = "insert"
+            elif a==alg.swap:
+                n = "swap"
+            
+            f_w = open(str('asym_tabu_'+n+'.txt'),"a")
             sol = alg.tabu_search(curr_graph,type_a=a)
             write_to_file(f_w,size,utils.objective(sol))
 
@@ -52,18 +54,18 @@ def test_start():
         alg.k_random,
         alg._2_opt
     ]
-    for a in algs:
-        if a == alg.nearest_neighbour:
-            n = "nearest"
-        elif a==alg.repetitive_nearest_neighbour:
-            n = "repetitive"
-        elif a==alg.k_random:
-            n = "random"
-        elif a==alg._2_opt:
-            n = "opt"
-        f_w = open(str('tabu_'+n+'.txt'),"w")
-        for size in [10,20,30,40]:
-            curr_graph = gen.generate_symmetric(size,1000)
+    for size in [10,20,30,40,50,60,70,80,90,100]:
+        curr_graph = gen.generate_symmetric(size,1000)
+        for a in algs:
+            if a == alg.nearest_neighbour:
+                n = "nearest"
+            elif a==alg.repetitive_nearest_neighbour:
+                n = "repetitive"
+            elif a==alg.k_random:
+                n = "random"
+            elif a==alg._2_opt:
+                n = "opt"
+            f_w = open(str('tabu_'+n+'.txt'),"a")
             sol = alg.tabu_search(curr_graph,start_type=a)
             write_to_file(f_w,size,utils.objective(sol))
 
@@ -71,10 +73,10 @@ def test_start():
 def test_tabu_size():
     starter = alg.nearest_neighbour
     operation = alg.invert
-    for size in [10,20,50,75]:
-        f_w = open(str('tabu_list_size_'+size+'.txt'),'w')
-        for tabu_size in [2,5,7,11,23,41,100,200]:
-            curr_graph = gen.generate_symmetric(size,1000)
+    for size in [20,50,100]:
+        curr_graph = gen.generate_symmetric(size,1000)
+        f_w = open(str('tabu_list_size_'+str(size)+'.txt'),'a')
+        for tabu_size in [1,3,6,12,25,50,100]:
             sol = alg.tabu_search(curr_graph,start_type=starter,size=tabu_size)
             write_to_file(f_w,tabu_size,utils.objective(sol))
 
@@ -82,16 +84,52 @@ def test_tabu_size():
 def test_tabu_graph():
     starter = alg.nearest_neighbour
     operation = alg.invert
-    f_s = open(str('tabu_graph_test_solution.txt'),'w')
-    f_t = open(str('tabu_graph_test_time.txt'),'w')
-    for size in [10,20,50,75,100]:
+    f_s = open(str('astabu_graph_test_solution.txt'),'a')
+    f_t = open(str('astabu_graph_test_time.txt'),'a')
+
+    f_s2 = open(str('as2opt_graph_test_solution.txt'),'a')
+    f_t2 = open(str('as2opt_graph_test_time.txt'),'a')
+
+    f_s3 = open(str('asrnn_graph_test_solution.txt'),'a')
+    f_t3 = open(str('asrnn_graph_test_time.txt'),'a')
+
+    f_s4 = open(str('asr_graph_test_solution.txt'),'a')
+    f_t4 = open(str('asr_graph_test_time.txt'),'a')
+
+    f_s5 = open(str('ask_graph_test_solution.txt'),'a')
+    f_t5 = open(str('ask_graph_test_time.txt'),'a')
+    for size in [10,20,30,40,50,60,70,80,90,100]:
        
-        curr_graph = gen.generate_symmetric(size,1000)
+        curr_graph = gen.generate_asymmetric(size,1000)
         start = time.time()       
-        sol = alg.tabu_search(curr_graph,start_type=starter,size=7,type_a=operation) #dalem rozmiar tabu listy 7, ale jak w testach wyjdzie jakis inny optymalny to mozna zmienic
+        sol = alg.tabu_search(curr_graph,start_type=starter,size=12,type_a=operation) 
         t = time.time()-start
         write_to_file(f_s,size,utils.objective(sol))
         write_to_file(f_t,size,t)
+
+        start = time.time()       
+        sol = alg._2_opt(curr_graph) 
+        t = time.time()-start
+        write_to_file(f_s2,size,utils.objective(sol))
+        write_to_file(f_t2,size,t)
+
+        start = time.time()       
+        sol = alg.repetitive_nearest_neighbour(curr_graph)
+        t = time.time()-start
+        write_to_file(f_s3,size,utils.objective(sol))
+        write_to_file(f_t3,size,t)
+
+        start = time.time()       
+        sol = alg.nearest_neighbour(curr_graph)
+        t = time.time()-start
+        write_to_file(f_s4,size,utils.objective(sol))
+        write_to_file(f_t4,size,t)
+
+        start = time.time()       
+        sol = alg.k_random(curr_graph, 100) 
+        t = time.time()-start
+        write_to_file(f_s5,size,utils.objective(sol))
+        write_to_file(f_t5,size,t)
 
 def test_all(G):
     algorithms = [
@@ -106,7 +144,12 @@ def test_all(G):
         print(utils.objective(algorithm(G)))
 
 if __name__=='__main__':
-    random.seed(150)
-    #G = gen.generate_asymmetric(30, 1000)
-    #test_all(G)
-    test_start()
+    random.seed(170)
+    G = gen.generate_symmetric(10,1000)
+    #sol = alg.repetitive_nearest_neighbour(G)
+    #print(utils.objective(sol))
+    #test_alg_type()
+    #test_start()
+    #test_tabu_graph()
+    #test_tabu_size()
+    gene.generate_population(G,alg=alg.k_random)
